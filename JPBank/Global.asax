781b7341ad59e13b782d6e5f726812d0da31e9d2
sean <sean@SEANPARK8528>
2017-03-02 15:53:39 -0800

    Add project files.

---
+++ JPBank/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JPBank.MvcApplication" Language="C#" %>
---
+++ JPBank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JPBank.Startup))]
+namespace JPBank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
